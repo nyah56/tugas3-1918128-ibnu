0af8dec6029d2f3330c1108b781d8c64d0cf28dd
nyah56 <x6perb@gmail.com>
2021-10-22 14:11:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/navbar_praktikum/HomeFragment.java
@@ -0,0 +1,22 @@
+package com.example.navbar_praktikum;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle
+                                     savedInstanceState) {
+        return
+                inflater.inflate(R.layout.frag_home,container,false);
+    }
+}
